617b830fb7de4169599a1b8b70986365cf9b935a
tmsgod2 <tmsgod2@naver.com>
2019-08-31 15:24:21 +0900

    if else event

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const title = document.querySelector("#title");
+let i = 0;
+const BASE_COLOR = "black";
+const OTHER_COLOR = "skyblue";
 
-title.innerText = "Hi! From JS";
-title.style.color = "#332133";
-document.title = "I own you now";
+function handleClick() {
+  const currentColor = title.style.color;
+  i++;
+  if (currentColor === BASE_COLOR) title.style.color = OTHER_COLOR;
+  else title.style.color = BASE_COLOR;
+  if (i > 10) title.innerHTML = "Stop it!!";
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("mouseenter", handleClick);
+}
+init();
