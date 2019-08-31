bc7a15abde874f3557a0b5d5f083a5f95f97978b
tmsgod2 <tmsgod2@naver.com>
2019-08-31 15:42:41 +0900

    classlist

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const title = document.querySelector("#title");
-let i = 0;
-const BASE_COLOR = "black";
-const OTHER_COLOR = "skyblue";
+
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  i++;
-  if (currentColor === BASE_COLOR) title.style.color = OTHER_COLOR;
-  else title.style.color = BASE_COLOR;
-  if (i > 10) title.innerHTML = "Stop it!!";
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
-  title.addEventListener("mouseenter", handleClick);
+  title.addEventListener("click", handleClick);
 }
 init();
